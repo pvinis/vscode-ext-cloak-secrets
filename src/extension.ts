40d27678352d40379921af1534236f56d6c2f783
Pavlos Vinieratos <pvinis@gmail.com>
2025-04-14 17:20:12 +0300

    aha

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,35 @@
+import * as vscode from "vscode";
 
 let statusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Cloak Secrets is now active.');
+  console.log("Cloak Secrets is now active.");
 
-  statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
   context.subscriptions.push(statusBarItem);
+
+  updateStatusBarItem();
+
+  const disposable = vscode.commands.registerCommand(
+    "cloak-secrets.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from cloak-secrets!");
+    }
+  );
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
+
+export function areSecretsCloaked() {
+}
 
 export function updateStatusBarItem() {
   const icon = areSecretsCloaked() ? `$(eye-closed)` : `$(eye)`;
-  const text = areSecretsCloaked() ? 'Uncloak' : 'Cloak';
-  const tooltip = areSecretsCloaked() ? 'Uncloak secrets' : 'Cloak secrets';
+  const text = areSecretsCloaked() ? "Uncloak" : "Cloak";
+  const tooltip = areSecretsCloaked() ? "Uncloak secrets" : "Cloak secrets";
 
   statusBarItem.text = `${icon} ${text}`;
   statusBarItem.tooltip = tooltip;
